7e9a634c4beea0026660cf919f1cbeec785adcd5
Mehar Goli <mehargoli@Mehars-MacBook-Pro-2.local>
2020-04-13 12:14:52 -0700

    <ForwardBackward.py> Implemented Forward Calulations: Wrote out algorithm skeleton, and implemented forward portion of algorithm routine - adding related helper functions accordingly.

--- hw7/handout/forwardbackward.py
+++ hw7/handout/forwardbackward.py
@@ -46,8 +46,6 @@ def initialize_reference(file_path):
         reader = reader.replace("\r", "")
         reference = reader.split('\n')
 
-        print(reference)
-
     return reference
 
 """ 
@@ -59,7 +57,7 @@ def initialize_reference(file_path):
 def initialize_prob_matrix(file_path):
     mat = np.loadtxt(file_path, delimiter = " ")
     
-    return mat
+    return np.transpose(mat)
 """ ---------------------------------------------------------------------------
                             Helper Functions
 --------------------------------------------------------------------------- """
@@ -73,8 +71,14 @@ Counts how many predicted tag values in sentence match the actual tags
 @param[in] sentence - list of actual word,tag tuples
 @return count - the number of correct predictions
 """
-def num_correct(pred, sentence):
-    return
+def count_correct(pred, sentence):
+    count = 0
+
+    for i in range(0, len(pred)):
+        if pred[i] == sentence[i][1]: #actual y tag is second elem in tuple
+            count += 1
+
+    return count
 
 """
 @brief: Determines matrix index of given word/tag
@@ -83,8 +87,9 @@ def num_correct(pred, sentence):
 @parm[in] reference - reference with words/tags and indexes
 @returns index of word/tag in reference
 """
-def value_to_tag(value, reference):
-    return
+def value_to_index(value, reference):
+    i = reference.index(value)
+    return i
 
 """
 @brief: Determines word/tag value of given matrix index
@@ -93,8 +98,9 @@ def value_to_tag(value, reference):
 @parm[in] reference - reference with words/tags and indexes
 @returns word/tag at index in reference
 """
-def tag_to_value(value, reference):
-    return
+def index_to_value(index, reference):
+    v = reference[index]
+    return v
 
 """ ---------------------------------------------------------------------------
                         Forward/Backward Routines
@@ -108,8 +114,16 @@ def tag_to_value(value, reference):
 @param[in] prior_mat - prior matrix
 @return alpha - generated alpha matrix
 """
-def forward(sentence, emm_mat, trans_mat, prior_mat):
-    return
+def forward(alpha, sentence, emit_mat, trans_mat, prior_mat, word_ref):
+    m = value_to_index(sentence[0][0], word_ref)
+    alpha[0] = np.multiply(prior_mat, emit_mat[m])
+
+    for t in range(1, len(sentence)):
+        m = value_to_index(sentence[t][0], word_ref)
+        alpha[t] = np.matmul(trans_mat, alpha[t-1])
+        alpha[t] = np.multiply(alpha[t], emit_mat[m])
+
+    return alpha
 
 """
 @brief: Runs backward routines for HMM
@@ -119,7 +133,7 @@ def forward(sentence, emm_mat, trans_mat, prior_mat):
 @param[in] trans_mat - transition matrix
 @return beta - generated beta matrix
 """
-def backward(sentence, emm_mat, trans_mat):
+def backward(beta, sentence, emit_mat, trans_mat, word_ref):
     return
 """ ---------------------------------------------------------------------------
                         Log-Likelihood Calculations
@@ -196,8 +210,10 @@ def initialize(data):
     data.prior_mat = initialize_prob_matrix(data.prior_inpath)
     data.emit_mat = initialize_prob_matrix(data.emit_inpath)
     data.trans_mat = initialize_prob_matrix(data.trans_inpath)
-# Read in A, B, pi
-# Read in X, Y, test data
+
+    data.M = len(data.word_ref) #Number of possible words
+    data.G = len(data.tag_ref) #Number of possible tags
+    data.N = len(data.test_data) #Number of sentences in set
 
 """
 @brief: Runs main forward backward algorithm
@@ -205,6 +221,29 @@ def initialize(data):
 @param[in] data - object containing all elements
 """
 def run_forward_backward_alg(data):
+    total_log_likelihood = 0
+    num_correct = 0
+    total = 0
+
+    for sentence in data.test_data:
+        T  = len(sentence)
+        alpha = np.zeros((T,data.G))
+        beta = np.zeros((T,data.G))
+
+        alpha = forward(alpha, sentence, data.emit_mat, data.trans_mat, data.prior_mat, data.word_ref)
+    #     beta = backward(beta, sentence, data.emit_mat, data.trans_mat, data.word_ref)
+
+    #     prob_mat = np.multiply(alpha, beta) #multiply elementwise
+    #     total_log_likelihood += log_likelihood(alpha[T-1])
+
+    #     pred = predict(prob_mat, data.tag_ref)
+    #     write_pred(pred, sentence, data.predictions_outpath)
+    #     num_correct += count_correct(pred, sentence)
+    #     total += T
+
+    # average_log_likelihood = total_log_likelihood/N
+    # error = num_correct / total
+    # write_metrics(average_log_likelihood, error, data.metrics_outpath)
     return
 
 """ ---------------------------------------------------------------------------
